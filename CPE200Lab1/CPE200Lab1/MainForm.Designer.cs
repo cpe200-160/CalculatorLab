84d1a9fa653a3300a4d616c1523f76440718f830
Pruet Boonma <pruetboonma@gmail.com>
2017-08-22 11:29:57 +0700

    Rename the placeholding form to MainForm.

--- CPE200Lab1/CPE200Lab1/MainForm.Designer.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CPE200Lab1
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- CPE200Lab1/CPE200Lab1/MainForm.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace CPE200Lab1
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
         private bool hasDot;
         private bool isAllowBack;
@@ -67,7 +67,7 @@ namespace CPE200Lab1
             return "E";
         }
 
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
 
