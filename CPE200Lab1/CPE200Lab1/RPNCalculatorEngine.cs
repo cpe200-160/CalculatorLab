ef6b680b955902e4bd39fb39ac0134055e7d9027
chawitW <beck.bfts@gmail.com>
2018-09-14 17:18:32 +0700

    Starting RPN

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -10,8 +10,33 @@ namespace CPE200Lab1
     {
         public string Process(string str)
         {
+            Stack<string> stack = new Stack<string>();
+            string[] parts = str.Split(' ');
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if ((isNumber(parts[i]) && isNumber(parts[i+1]) && isOperator(parts[i+2])))
+                {
+                    return calculate(parts[i + 2], parts[i], parts[i + 1], 4);
+                }
+                else
+                {
+                    string firstOperand, secondOperand;
+                    stack.Push(parts[i]);
+
+                    string next = calculate(parts[i + 3], parts[i+1], parts[i + 2], 4);
+                    stack.Push(next);
+
+                    secondOperand = stack.Pop();
+                    firstOperand = stack.Pop();
+
+                    return calculate(parts[i + 2], firstOperand, secondOperand, 4);
+
+                }
+                
+            }
+
+            return stack.Pop();
             
-            return "E";
         }
     }
 }
