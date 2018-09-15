f3b3dc924801738958a0c752d5d66d9c97e2cb68
thipaporn <35359046+thipaporn@users.noreply.github.com>
2018-09-15 17:26:47 +0700

    แก้ CalculatorEngine

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -10,7 +10,15 @@ namespace CPE200Lab1
     {
         public string Process(string str)
         {
-            
+            string[] parts = str.Split(' ');
+            if (!(isNumber(parts[0]) && isOperator(parts[1]) && isNumber(parts[2])))
+            {
+                return "E";
+            }
+            else
+            {
+                return calculate(parts[1], parts[0], parts[2], 4);
+            }
             // your code here
             return "E";
         }
