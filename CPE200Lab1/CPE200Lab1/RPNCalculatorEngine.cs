6c103fb351ab756d69817c5ba711719631332700
KanapongP <35338845+KanapongP@users.noreply.github.com>
2018-09-20 23:22:50 +0700

    fix error process in error test && COMPLETE

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,12 +6,35 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
         public string Process(string str)
         {
             // your code here
-            return "E";
+            Stack<string> rpnStack = new Stack<string>();
+            string[] parts = str.ToString().Split(' ');
+           bool check = false;
+            foreach (string part in parts)
+            {
+                if (part != "+" && part != "-" && part != "X" && part != "÷")
+                {
+                    
+                    rpnStack.Push(part);
+                }
+                else 
+                {
+                    check = true;
+                    string firstOperand, secondOperand;
+                    if (rpnStack.Count < 2) return "E";
+                    secondOperand = rpnStack.Pop();
+                    firstOperand = rpnStack.Pop();
+                    rpnStack.Push(calculate(part, firstOperand, secondOperand)); 
+                }
+                
+            }
+            if (check == false|| rpnStack.Count > 1) return "E";
+            check = false;
+            return rpnStack.Pop();
         }
     }
 }
