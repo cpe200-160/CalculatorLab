6fca17555c38070b05193c9bfeae0f2651c0212f
KanapongP <35338845+KanapongP@users.noreply.github.com>
2018-09-20 20:44:06 +0700

    solve test case in calculator engine

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -59,7 +59,7 @@ namespace CPE200Lab1
                         // calculate remaining space for fractional part.
                         remainLength = maxOutputSize - parts[0].Length - 1;
                         // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
+                        return result.ToString(/*"N" + remainLength*/);
                     }
                 case "1/x":
                     if(operand != "0")
@@ -79,7 +79,7 @@ namespace CPE200Lab1
                         // calculate remaining space for fractional part.
                         remainLength = maxOutputSize - parts[0].Length - 1;
                         // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
+                        return result.ToString(/*"N" + remainLength*/);
                     }
                     break;
             }
@@ -115,7 +115,7 @@ namespace CPE200Lab1
                         // calculate remaining space for fractional part.
                         remainLength = maxOutputSize - parts[0].Length - 1;
                         // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
+                        return result.ToString(/*"N" + remainLength*/);
                     }
                     break;
                 case "%":
