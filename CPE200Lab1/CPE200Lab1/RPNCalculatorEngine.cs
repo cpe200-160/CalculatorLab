5750c48cff7b693a736ea34e81353536696c6b04
PATTADON NUTES_udate <pattadon_nutes@cmu.ac.th>
2018-10-18 17:03:23 +0700

    LoL

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -12,64 +12,75 @@ namespace CPE200Lab1
 
 		public override string Process(string str)
 		{
-
-
-			string[] parts = str.Split(' ');
-			Stack<string> operands = new Stack<string>();
-			string result ="0";
-			Boolean Key = false;
-			int amountNum = 0 , amountOper = 0;
-
-			for (int i = 0; i < parts.Length; i++)
+			Boolean w = true;
+			int ValueNum = 0;
+			for (int j = 0;j < str.Length; j++)
 			{
-				
-				if(parts[i] == "1+"|| parts[i] == "+1"||parts[i] =="++")
+				if(str == null)
 				{
+					w = false;
 					return "E";
 				}
-				if (isNumber(parts[i]))
+			}
+
+				string[] parts = str.Split(' ');
+				Stack<string> operands = new Stack<string>();
+				string result = "0";
+				Boolean Key = false;
+				int amountNum = 0, amountOper = 0;
+		
+				for (int i = 0; i < parts.Length; i++)
 				{
-					if(Double.Parse(parts[i]) >= 1)
-					operands.Push(parts[i]);
-					result = parts[i];
-					amountNum = amountNum + 1;
-				}
-				else if (isOperator(parts[i]))
-				{
-					string ppap1 = "0";
-					string ppap2 = "0";
-					if (operands.Count() > 1)
+
+					if (parts[i] == "1+" || parts[i] == "+1" || parts[i] == "++" || parts[i] == "\"\"")
 					{
-						ppap2 = operands.Pop();
-						ppap1 = operands.Pop();
+						return "E";
+					}
+					if (isNumber(parts[i]))
+					{
+						if (Double.Parse(parts[i]) >= 1)
+							operands.Push(parts[i]);
+						result = parts[i];
+						amountNum = amountNum + 1;
+					    ValueNum++;
+					}
+					else if (isOperator(parts[i]))
+					{
+						string ppap1 = "0";
+						string ppap2 = "0";
+						if (operands.Count() > 1)
+						{
+							ppap2 = operands.Pop();
+							ppap1 = operands.Pop();
+
+						}
+						else
+						{
+							return "E";
+						}
+
+						result = calculate(parts[i], ppap1, ppap2, 4);
+						operands.Push(result);
+						Key = true;
+						amountOper++;
 
 					}
-					else
+					else if (parts[i] == null)
 					{
 						return "E";
 					}
 
-					result = calculate(parts[i], ppap1, ppap2, 4);
-					operands.Push(result);
-					Key = true;
-					amountOper++;
 
 				}
-				else if (parts[i] == null)
+				if (amountNum - amountOper != 1 && Key == true)
 				{
 					return "E";
 				}
-
-	
-			}
-			if(amountNum - amountOper != 1)
-			{
-				return "E";
-			}
-			if(Key == false)
-			{
-				return "E";
-			}
+				if (Key == false && ValueNum > 1)
+				{
+					return "E";
+				}
+			
 
 			return result;
 		}
