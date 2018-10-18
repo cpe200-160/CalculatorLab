b9b6b2b43794d36ba96a5d3575da203d9fb1834e
PATTADON NUTES_udate <pattadon_nutes@cmu.ac.th>
2018-10-18 16:34:37 +0700

    update why

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -5,53 +5,73 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CPE200Lab1
+
 {
-    public class RPNCalculatorEngine : CalculatorEngine
-    {
-        public override string Process(string str)
-        {
+	public class RPNCalculatorEngine : CalculatorEngine
+	{
 
-            string[] parts = str.Split(' ');
-            Stack<string> operands = new Stack<string>();
-            string result = "0";
-            for (int i = 0; i < parts.Length; i++)
-            {
-                if (isNumber(parts[i]))
-                {
-                    operands.Push(parts[i]);
-                    if (i == 0)
-                    {
-                        result = parts[i];
-                    }
-                }
-                else if (isOperator(parts[i]))
-                {
-                   string ppap1;
-                   string ppap2;
-                    int amountOperads = operands.Count();
-                    if (operands.Count() > 1 )
-                    {
-                        if (amountOperads >= 3)
-                        {
-                            continue;
-                        }
-                        ppap2 = operands.Pop();
-                        ppap1 = operands.Pop();
+		public override string Process(string str)
+		{
 
-                    }else
-                    {
-                      return "E";
-                    }
-                    result = calculate(parts[i], ppap1, ppap2, 4);
-                    operands.Push(result);
-                }
-                else
-                {
-                    
-                }
-            }
-            return result;
-        }
 
-    }
+			string[] parts = str.Split(' ');
+			Stack<string> operands = new Stack<string>();
+			string result ="0";
+			Boolean Key = false;
+			int amountNum = 0 , amountOper = 0;
+
+			for (int i = 0; i < parts.Length; i++)
+			{
+				
+				if(parts[i] == "1+"|| parts[i] == "+1"||parts[i] =="++")
+				{
+					return "E";
+				}
+				if (isNumber(parts[i]))
+				{
+					if(Double.Parse(parts[i]) >= 1)
+					operands.Push(parts[i]);
+					result = parts[i];
+					amountNum = amountNum + 1;
+				}
+				else if (isOperator(parts[i]))
+				{
+					string ppap1 = "0";
+					string ppap2 = "0";
+					if (operands.Count() > 1)
+					{
+						ppap2 = operands.Pop();
+						ppap1 = operands.Pop();
+
+					}
+					else
+					{
+						return "E";
+					}
+
+					result = calculate(parts[i], ppap1, ppap2, 4);
+					operands.Push(result);
+					Key = true;
+					amountOper++;
+
+				}
+				else if (parts[i] == null)
+				{
+					return "E";
+				}
+
+	
+			}
+			if(amountNum - amountOper != 1)
+			{
+				return "E";
+			}
+			if(Key == false)
+			{
+				return "E";
+			}
+
+			return result;
+		}
+	}
 }
